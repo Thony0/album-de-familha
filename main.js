aa87859b8e1dcc87807f2fe31e799ccab29abaf5
Thony0 <111467113+Thony0@users.noreply.github.com>
2022-12-20 21:57:47 -0300

    Add files via upload
    sjdnaksdnasjdalsjdakjshdaksjhd

---
+++ main.js
@@ -0,0 +1,14 @@
+var i=0;
+function update()
+{
+    i++;
+}
+var numeroDasFrutasNoArray=5
+if(i > numerosDasFrutasNoArray )
+{
+    i=0;
+}
+var atualizarImagem=imagems[i];
+var atualizarNome=nome[i];
+document.getElementById(frutasImagen).src=atualizarImagem
+document.getElementById(frutasNome).src=atualizarINome
